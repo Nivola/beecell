868e3436375c7bc13d4247aa2398554b0192b0f6
1362 <sergio.tonani@csi.it>
2016-07-14 17:08:36 +0200

    correzion bug

--- beecell/db/__init__.py
+++ beecell/db/__init__.py
@@ -4,9 +4,13 @@ class TransactionError(Exception):
         """
         self.desc = desc
         self.code = code
+        Exception.__init__(self, desc)
         
+    def __repr__(self):
+        return "TransactionError: %s" % self.desc
+
     def __str__(self):
-        return "(TransactionError) (%s, %s)" % (self.code, self.desc)    
+        return "TransactionError: %s" % self.desc
     
 class QueryError(Exception):
     def __init__(self, desc, code=0):
@@ -14,8 +18,12 @@ class QueryError(Exception):
         """
         self.desc = desc
         self.code = code
+        Exception.__init__(self, desc)
         
+    def __repr__(self):
+        return "QueryError: %s" % self.desc
+
     def __str__(self):
-        return "(QueryError) (%s, %s)" % (self.code, self.desc)
+        return "QueryError: %s" % self.desc
 
 from .manager import MysqlManager
