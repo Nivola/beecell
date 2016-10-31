79c141fc69ff12d4603009ba4e718233a4ae119b
1362 <sergio.tonani@csi.it>
2016-10-31 18:21:05 +0100

    rivisto metodo gestione tmeplate flask

--- beecell/flask/render.py
+++ beecell/flask/render.py
@@ -9,12 +9,29 @@ from flask_login import current_user
 
 logger = logging.getLogger(__name__)
 
-def render_template(url, *argc, **argv):
+def _inner_render(url, *argc, **argv):
+    """ """
     try:
-        url_profile = '%s.%s' % (url, current_user.get_profile())
+        res = flask_render_template(url, *argc, **argv)
+    except Exception as ex:
+        logger.warn(u'Template %s error' % (url), exc_info=1)
+        res = u''
+    
+    return res 
+
+def render_template(url, *argc, **argv):
+    """ """
+    ver = None
+    try:
+        ver = current_user.get_portal_version()
+        # find the last . in template string
+        urllist = url.split(u'.')
+        urllist.insert(-1, ver)
+        url_profile = u'.'.join(urllist)
+        #url_profile = '%s.%s' % (ver, url)
         res = flask_render_template(url_profile, *argc, **argv)
-        logger.debug('Use profile template: %s' % url_profile)
+        logger.debug(u'Use template %s version %s' % (url, ver))
         return res
     except Exception as ex:
-        logger.debug('Use default template: %s' % url)
+        logger.warn(u'Template %s version %s not found. Use default' % (url, ver))
         return flask_render_template(url, *argc, **argv)
