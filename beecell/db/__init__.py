4399b20a3bd1c134b0c7fda62f4fafeb204c3d73
1362 <sergio.tonani@csi.it>
2017-01-30 18:30:06 +0100

    correzione bug a aggiunti metodi manager

--- beecell/db/__init__.py
+++ beecell/db/__init__.py
@@ -1,18 +1,4 @@
 class ModelError(Exception):
-    def __init__(self, desc, code=0):
-        """
-        """
-        self.desc = desc
-        self.code = code
-        Exception.__init__(self, desc)
-        
-    def __repr__(self):
-        return "ModelError: %s" % self.desc
-
-    def __str__(self):
-        return u'%s, %s' % (self.desc, self.code)
-
-class ModelError(Exception):
     def __init__(self, desc, code=404):
         """
         """
@@ -38,7 +24,7 @@ class TransactionError(Exception):
         return "TransactionError: %s" % self.desc
 
     def __str__(self):
-        return u'%s, %s' % (self.desc, self.code)
+        return self.desc
     
 class QueryError(Exception):
     def __init__(self, desc, code=0):
@@ -52,6 +38,6 @@ class QueryError(Exception):
         return "QueryError: %s" % self.desc
 
     def __str__(self):
-        return u'%s, %s' % (self.desc, self.code)
+        return self.desc
 
 from .manager import MysqlManager
