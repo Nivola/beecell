91b1027c3fc17dbcf9fca2edf473855d00c1836e
1362 <sergio.tonani@csi.it>
2018-11-04 19:44:45 +0100

    pulizia package

--- examples/parse_data.py
+++
@@ -1,7 +0,0 @@
-from beecell.simple import obscure_data
-
-data = {u'pwd': u'ddhednuhwe', u'name': u'pospso',
-        u'other': {u'passwd': u'ddhednuhwe', u'name': u'pospso'},
-        u'others': {u'password': u'ddhednuhwe', u'name': u'pospso'}}
-
-print obscure_data(data)
