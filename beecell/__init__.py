8cff1c9ca7bc041834ac0af8cdf8fc9433b7b111
1362 <sergio.tonani@csi.it>
2018-11-27 13:29:29 +0100

    aggiunta gestione version del package

---
+++ beecell/__init__.py
@@ -0,0 +1,7 @@
+__version__ = u'1.0.0'
+
+import os.path
+version_file = os.path.join(os.path.abspath(__file__).rstrip(u'__init__.pyc'), u'VERSION')
+if os.path.isfile(version_file):
+    with open(version_file) as version_file:
+        __version__ = u'%s-%s' % (__version__, version_file.read().strip()[:10])
