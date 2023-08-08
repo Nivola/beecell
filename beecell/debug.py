a9a92add4b4b784f42519d38309bb29087e436dc
72386 <gianni.doria@consulenti.csi.it>
2023-08-08 16:05:53 +0200

    new environment vars for checke and info

--- beecell/debug.py
+++ beecell/debug.py
@@ -3,6 +3,7 @@
 # (C) Copyright 2018-2022 CSI-Piemonte
 import traceback
 import os
+import sys
 import logging
 CODE_DBG_PORT = 5678
 
@@ -37,74 +38,45 @@ def init_debugger():
 def debug_cmp():
     """Check configurations for debugging and enable debug if needed"""
     # debugpy_enabled = uwsgi_util.opt.get("debugpy_enabled", None)
-    debugpy_enabled = True
 
+    debugpy_uwsgi = os.environ.get("DEBUG_UWSGI", "NO")== "YES"
+    debugpy_worker = os.environ.get("DEBUG_WORKER", "NO")== "YES"
+    debugpy_enabled = debugpy_uwsgi or debugpy_worker
+    ####################################
     # if debugpy_enabled != None and debugpy_enabled and not bool(uwsgi_util.opt.get("master", False)):
-    if os.environ.get("DEBUG_ENABLED")== "YES":
-        import sys
-        import time
-        import threading
-        import yaml
+    if debugpy_enabled:
+        dbgport: int = int (os.environ.get("DEBUG_PORT", 5680))
+        dbgtimeout: int = int (os.environ.get("DEBUG_TIMEOUT", 120))
+        # 1-ENABLE GEVENT SUPPORT
+        os.environ["GEVENT_SUPPORT"] = "True"
+        # 2-GET POD NAME
+        hostname = os.environ["HOSTNAME"]
+        try:
+            # 4- SETUP A LOGGER FOR DEBUGPY
+            debugpy_logger = logging.getLogger("debugpy")
+            debugpy_logger.setLevel(logging.DEBUG)
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            debugpy_logger.addHandler(stdout_handler)
+            # 5- IMPORTING MODULE
+            debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ - Importing module...")
+            import debugpy
 
-        ####################################
-        # TODO valutare se spostare su un configmap con un file YAML
-        ####################################
-        debugpy_yaml = yaml.safe_load(
-            """
-            debugpy:
-              enabled: true
-              subsystems:
-                - name: auth
-                  port: 5678
-                  timeout_connection: 60
-                - name: service
-                  port: 5679
-                  timeout_connection: 120
-                - name: resource
-                  port: 5680
-                  timeout_connection: 120
-            """
-        )
-        debugpy_opt = debugpy_yaml.get("debugpy", {})
-        if bool(debugpy_opt.get("enabled", False)) and debugpy_opt.get("subsystems", None) != None:
-            # 1-ENABLE GEVENT SUPPORT
-            os.environ["GEVENT_SUPPORT"] = "True"
-            # 2-GET POD NAME
-            hostname = os.environ["HOSTNAME"]
-            for subsystem in debugpy_opt["subsystems"]:
-                # 3- IS SUBSYSTEM ENABLED FOR LOGGING?
-                if (
-                    subsystem != None
-                    and hostname != None
-                    and subsystem.get("name", None) != None
-                    and subsystem.get("port", None) != None
-                    and f'uwsgi-{subsystem["name"]}' in hostname
-                ):
-                    try:
-                        # 4- SETUP A LOGGER FOR DEBUGPY
-                        debugpy_logger = logging.getLogger("debugpy")
-                        debugpy_logger.setLevel(logging.DEBUG)
-                        stdout_handler = logging.StreamHandler(sys.stdout)
-                        debugpy_logger.addHandler(stdout_handler)
-                        # 5- IMPORTING MODULE
-                        debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ - Importing module...")
-                        import debugpy
+            debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ debugging mode ENABLED for POD %s", hostname)
+            debugpy.listen(("0.0.0.0", dbgport ))
+            debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ Listening at 0.0.0.0:%s 🤔 ❌", dbgport )
+            # 6- TASK TIME FOR WAITING CLIENT TO CONNECT
+            # debugpy.wait_for_client()
+            # class T(threading.Thread):
+            #     def run(self):
+            #         timeout = subsystem.get('timeout_connection', '120')
+            #         time.sleep(float(timeout))
+            #         debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ Cancelling debug wait task after %s sec of waiting client to connect ❌", timeout)
+            #         debugpy.wait_for_client.cancel()
+            # T().start()
+            debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ Debugger Client Connected 🤓 ✅")
+        except RuntimeError:
+            debugpy_logger.error("🐞⚡️debugpy⚡️ ❱❱❱ - Got Runtime error.")
 
-                        debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ debugging mode ENABLED for POD %s", hostname)
-                        debugpy.listen(("0.0.0.0", subsystem["port"]))
-                        debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ Listening at 0.0.0.0:%s 🤔 ❌", subsystem["port"])
-                        # 6- TASK TIME FOR WAITING CLIENT TO CONNECT
-                        # debugpy.wait_for_client()
-                        # class T(threading.Thread):
-                        #     def run(self):
-                        #         timeout = subsystem.get('timeout_connection', '120')
-                        #         time.sleep(float(timeout))
-                        #         debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ Cancelling debug wait task after %s sec of waiting client to connect ❌", timeout)
-                        #         debugpy.wait_for_client.cancel()
-                        # T().start()
-                        debugpy_logger.debug("🐞⚡️debugpy⚡️ ❱❱❱ Debugger Client Connected 🤓 ✅")
-                    except RuntimeError:
-                        debugpy_logger.error("🐞⚡️debugpy⚡️ ❱❱❱ - Got Runtime error.")
 
 
 def dbgprint(*args, **kwargs):
