f8174d8f8374df32f891098eae4b2bffe63a1d82
2162 <davide.sacchetto@csi.it>
2024-11-27 17:44:35 +0100

    Changes to use encrypted passwords in env files

--- beecell/file.py
+++ beecell/file.py
@@ -4,24 +4,46 @@
 
 import os
 import xml.etree.ElementTree as et
-from yaml import full_load
-from six import b
-from ujson import loads
-
 # from xmltodict import parse as xmltodict
+from yaml import full_load
+from ujson import loads
+from base64 import urlsafe_b64decode
+from six import b, ensure_binary
+from beecell.crypto_util.fernet import Fernet
+from beecell.crypto import VAULT_MARK
 
 
-def read_file(file_name, parse=True):
+def check_encrypted(data, fernet: Fernet):
+    """
+    Iterate through data dictionary to look for values encrypted by fernet token and AES128, and decrypt them.
+
+    :param data: the dictionary to browse. Encrypted values start with '$BEEHIVE_VAULT;AES128 | '
+    :param fernet: fernet key
+    :return: the dictionary with decrypted values
+    """
+    for key, value in data.items():
+        if isinstance(value, dict):
+            check_encrypted(value, fernet)
+        elif isinstance(value, str) and value.startswith(VAULT_MARK):
+            value = ensure_binary(value)
+            value = value.replace(b(VAULT_MARK), b(""))
+            value = fernet.decrypt(value)
+            data[key] = value.decode("utf-8")
+    return data
+
+# def read_file(file_name, parse=True):
+def read_file(file_name: str, parse=True, secret: str = None) -> dict:
     """Load dict from a json or yaml formatted file.
 
     :param file_name: file name
     :param parse: if True parse file
+    :param secret: encryption/decryption key
     :return: data
     """
-    
     f = open(os.path.expanduser(file_name), "r")
     data = f.read()
     extension = file_name[-4:].lower()
+
     if parse is True:
         if extension == b("json") or extension == "json":
             data = loads(data)
@@ -32,4 +54,9 @@ def read_file(file_name, parse=True):
         elif extension == b(".xml") or extension == ".xml":
             data = et.fromstring(data)
     f.close()
+
+    if secret is not None:
+        fernet = Fernet(urlsafe_b64decode(secret))
+        data = check_encrypted(data, fernet)
+
     return data
